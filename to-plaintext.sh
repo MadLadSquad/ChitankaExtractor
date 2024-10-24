a0f38a019167f13127fa2dd4c67d2731b1ac4af6
Stanislav Vasilev <stanv.contact176@gmail.com>
2024-10-24 22:33:54 +0300

    Update to-plaintext.sh

--- to-plaintext.sh
+++ to-plaintext.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 convert_to_plaintext() {
 	result=$(grep "<p>" "$1" | sed -E "s/(<(\/?)p>)|(<span.*<\/span>)|(<em.*<\/em>)|(<li.*<\/li>)|(<(\/?)strong>)|(<a.*<\/a>)|(<.* \/>)|(<(\/?)sup>)//g" | head -n -3 | tail -n +4)
 	#sed -E "s/<a.*<\/a>//g" | sed -E "s/<em.*<\/em>//g" | sed -E "s/<li.*<\/li>//g" | sed -E "s/<>//g" | head -n -3 | tail -n +4)
